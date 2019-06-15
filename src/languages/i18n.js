3dd69da29b1e3661ffa75fe343c777b9914b1971
buivanhuu2017 <buivanhuu2017@gmail.com>
2019-06-15 07:53:48 +0700

    Update path languages

--- src/languages/i18n.js
+++ src/languages/i18n.js
@@ -1,14 +1,18 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import en from './en'
-import vi from './vi'
+import en from './en/resource.json'
+import vi from './vi/resource.json'
 
 i18n
   .use(initReactI18next)
   .init({
     resources: {
-      en,
-      vi
+      en: {
+        translation: en
+      },
+      vi : {
+        translation: vi
+      }
     },
     lng: "en",
     fallbackLng: "en",
--- src/languages/vi.js
+++
@@ -1,8 +0,0 @@
-const vi = {
-  translation: {
-    "Welcome to React": "Welcome to React and react-i18next - VI",
-    Dashboard: "Trang chủ"
-  }
-};
-
-export default vi;
