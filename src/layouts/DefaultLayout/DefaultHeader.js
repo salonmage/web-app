9fa8f83c6277bd9cc634fd24bc90d66f155f1723
buivanhuu2017 <buivanhuu2017@gmail.com>
2019-06-21 07:25:49 +0700

    Update react-i18next package

--- src/layouts/DefaultLayout/DefaultHeader.js
+++ src/layouts/DefaultLayout/DefaultHeader.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import {
   Badge,
@@ -9,7 +9,6 @@ import {
   NavItem,
   Dropdown
 } from "reactstrap";
-import PropTypes from "prop-types";
 
 import {
   AppAsideToggler,
@@ -19,134 +18,113 @@ import {
 import logo from "../../assets/img/brand/logo.svg";
 import sygnet from "../../assets/img/brand/sygnet.svg";
 
-const propTypes = {
-  children: PropTypes.node
-};
+function DefaultHeader(props) {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  return (
+    <React.Fragment>
+      <AppSidebarToggler className="d-lg-none" display="md" mobile />
+      <AppNavbarBrand
+        full={{ src: logo, width: 89, height: 25, alt: "CoreUI Logo" }}
+        minimized={{ src: sygnet, width: 30, height: 30, alt: "CoreUI Logo" }}
+      />
+      <AppSidebarToggler className="d-md-down-none" display="lg" />
 
-const defaultProps = {};
-
-class DefaultHeader extends Component {
-  state = {
-    dropdownOpen: false
-  };
-
-  toggle = () => {
-    this.setState({ dropdownOpen: !this.state.dropdownOpen });
-  };
-
-  render() {
-    // eslint-disable-next-line
-    const { children, ...attributes } = this.props;
-
-    return (
-      <React.Fragment>
-        <AppSidebarToggler className="d-lg-none" display="md" mobile />
-        <AppNavbarBrand
-          full={{ src: logo, width: 89, height: 25, alt: "CoreUI Logo" }}
-          minimized={{ src: sygnet, width: 30, height: 30, alt: "CoreUI Logo" }}
-        />
-        <AppSidebarToggler className="d-md-down-none" display="lg" />
-
-        <Nav className="d-md-down-none" navbar>
-          <NavItem className="px-3">
-            <NavLink to="/dashboard" className="nav-link">
-              Dashboard
-            </NavLink>
-          </NavItem>
-          <NavItem className="px-3">
-            <Link to="/users" className="nav-link">
-              Users
-            </Link>
-          </NavItem>
-          <NavItem className="px-3">
-            <NavLink to="#" className="nav-link">
-              Settings
-            </NavLink>
-          </NavItem>
-        </Nav>
-        <Nav className="ml-auto" navbar>
-          <NavItem className="d-md-down-none">
-            <NavLink to="#" className="nav-link">
-              <i className="icon-bell" />
-              <Badge pill color="danger">
-                5
-              </Badge>
-            </NavLink>
-          </NavItem>
-          <NavItem className="d-md-down-none">
-            <NavLink to="#" className="nav-link">
-              <i className="icon-list" />
-            </NavLink>
-          </NavItem>
-          <NavItem className="d-md-down-none">
-            <NavLink to="#" className="nav-link">
-              <i className="icon-location-pin" />
-            </NavLink>
-          </NavItem>
-          <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-            <DropdownToggle nav>
-              <img
-                src={"../../assets/img/avatars/6.jpg"}
-                className="img-avatar"
-                alt="admin@bootstrapmaster.com"
-                width="35"
-              />
-            </DropdownToggle>
-            <DropdownMenu>
-              <DropdownItem header tag="div" className="text-center">
-                <strong>Account</strong>
-              </DropdownItem>
-              <DropdownItem>
-                <i className="fa fa-bell-o" /> Updates
-                <Badge color="info">42</Badge>
-              </DropdownItem>
-              <DropdownItem>
-                <i className="fa fa-envelope-o" /> Messages
-                <Badge color="success">42</Badge>
-              </DropdownItem>
-              <DropdownItem>
-                <i className="fa fa-tasks" /> Tasks
-                <Badge color="danger">42</Badge>
-              </DropdownItem>
-              <DropdownItem>
-                <i className="fa fa-comments" /> Comments
-                <Badge color="warning">42</Badge>
-              </DropdownItem>
-              <DropdownItem header tag="div" className="text-center">
-                <strong>Settings</strong>
-              </DropdownItem>
-              <DropdownItem>
-                <i className="fa fa-user" /> Profile
-              </DropdownItem>
-              <DropdownItem>
-                <i className="fa fa-wrench" /> Settings
-              </DropdownItem>
-              <DropdownItem>
-                <i className="fa fa-usd" /> Payments
-                <Badge color="secondary">42</Badge>
-              </DropdownItem>
-              <DropdownItem>
-                <i className="fa fa-file" /> Projects
-                <Badge color="primary">42</Badge>
-              </DropdownItem>
-              <DropdownItem divider />
-              <DropdownItem>
-                <i className="fa fa-shield" /> Lock Account
-              </DropdownItem>
-              <DropdownItem onClick={e => this.props.onLogout(e)}>
-                <i className="fa fa-lock" /> Logout
-              </DropdownItem>
-            </DropdownMenu>
-          </Dropdown>
-        </Nav>
-        <AppAsideToggler className="d-md-down-none" />
-        {/*<AppAsideToggler className="d-lg-none" mobile />*/}
-      </React.Fragment>
-    );
-  }
+      <Nav className="d-md-down-none" navbar>
+        <NavItem className="px-3">
+          <NavLink to="/dashboard" className="nav-link">
+            Dashboard
+          </NavLink>
+        </NavItem>
+        <NavItem className="px-3">
+          <Link to="/users" className="nav-link">
+            Users
+          </Link>
+        </NavItem>
+        <NavItem className="px-3">
+          <NavLink to="#" className="nav-link">
+            Settings
+          </NavLink>
+        </NavItem>
+      </Nav>
+      <Nav className="ml-auto" navbar>
+        <NavItem className="d-md-down-none">
+          <NavLink to="#" className="nav-link">
+            <i className="icon-bell" />
+            <Badge pill color="danger">
+              5
+            </Badge>
+          </NavLink>
+        </NavItem>
+        <NavItem className="d-md-down-none">
+          <NavLink to="#" className="nav-link">
+            <i className="icon-list" />
+          </NavLink>
+        </NavItem>
+        <NavItem className="d-md-down-none">
+          <NavLink to="#" className="nav-link">
+            <i className="icon-location-pin" />
+          </NavLink>
+        </NavItem>
+        <Dropdown isOpen={dropdownOpen} toggle={() => setDropdownOpen(!dropdownOpen)}>
+          <DropdownToggle nav>
+            <img
+              src={"../../assets/img/avatars/6.jpg"}
+              className="img-avatar"
+              alt="admin@bootstrapmaster.com"
+              width="35"
+            />
+          </DropdownToggle>
+          <DropdownMenu>
+            <DropdownItem header tag="div" className="text-center">
+              <strong>Account</strong>
+            </DropdownItem>
+            <DropdownItem>
+              <i className="fa fa-bell-o" /> Updates
+              <Badge color="info">42</Badge>
+            </DropdownItem>
+            <DropdownItem>
+              <i className="fa fa-envelope-o" /> Messages
+              <Badge color="success">42</Badge>
+            </DropdownItem>
+            <DropdownItem>
+              <i className="fa fa-tasks" /> Tasks
+              <Badge color="danger">42</Badge>
+            </DropdownItem>
+            <DropdownItem>
+              <i className="fa fa-comments" /> Comments
+              <Badge color="warning">42</Badge>
+            </DropdownItem>
+            <DropdownItem header tag="div" className="text-center">
+              <strong>Settings</strong>
+            </DropdownItem>
+            <DropdownItem>
+              <i className="fa fa-user" /> Profile
+            </DropdownItem>
+            <DropdownItem>
+              <i className="fa fa-wrench" /> Settings
+            </DropdownItem>
+            <DropdownItem>
+              <i className="fa fa-usd" /> Payments
+              <Badge color="secondary">42</Badge>
+            </DropdownItem>
+            <DropdownItem>
+              <i className="fa fa-file" /> Projects
+              <Badge color="primary">42</Badge>
+            </DropdownItem>
+            <DropdownItem divider />
+            <DropdownItem>
+              <i className="fa fa-shield" /> Lock Account
+            </DropdownItem>
+            <DropdownItem onClick={e => props.onLogout(e)}>
+              <i className="fa fa-lock" /> Logout
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </Nav>
+      <AppAsideToggler className="d-md-down-none" />
+      {/*<AppAsideToggler className="d-lg-none" mobile />*/}
+    </React.Fragment>
+  );
 }
-
-DefaultHeader.propTypes = propTypes;
-DefaultHeader.defaultProps = defaultProps;
 
 export default DefaultHeader;
