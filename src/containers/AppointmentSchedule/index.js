308825e74bb825c78ed74808d9d592484e8022b6
buivanhuu2017 <buivanhuu2017@gmail.com>
2019-06-25 11:24:36 +0700

    Apppointment Schedule

--- src/containers/AppointmentSchedule/index.js
+++ src/containers/AppointmentSchedule/index.js
@@ -1,48 +1,71 @@
 import React, { useState } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
-import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from "reactstrap";
+import {
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  Input
+} from "reactstrap";
 
 const localizer = momentLocalizer(moment);
+const myEventsInit = [];
 
 function AppointmentSchedule() {
-  const myEventsList = [
-    {
-      start: new Date(),
-      end: new Date(moment().add(1, "days")),
-      title: "Some title"
-    }
-  ];
   const [modal, setModal] = useState(false);
   const [backdrop] = useState(true);
+  const [events, setEvents] = useState(myEventsInit);
+  const [eventSelect, setEventSelect] = useState(null);
+  const [title, setTitle] = useState("");
+
   function toggle() {
     setModal(!modal);
+  }
+
+  function handleSelectSlot(e) {
+    const event = {
+      start: e.start,
+      end: e.end
+    };
+    setEventSelect(event);
+    setModal(true);
+  }
+
+  function handleAddEvent() {
+    let eventSelectWithText = eventSelect;
+    eventSelectWithText.title = title;
+    setEvents(events => events.concat([eventSelectWithText]));
+    setEventSelect(null);
+    setTitle("");
+    setModal(false);
   }
 
   return (
     <>
       <Calendar
         localizer={localizer}
-        events={myEventsList}
+        events={events}
         defaultView="week"
         style={{ height: "50vh" }}
-        onSelectSlot={toggle}
+        onSelectSlot={handleSelectSlot}
         selectable={true}
+        onSelectEvent={e => console.log(e)}
       />
       <Modal isOpen={modal} toggle={toggle} backdrop={backdrop}>
-        <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+        <ModalHeader toggle={toggle}>Đặt lịch</ModalHeader>
         <ModalBody>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          <Input
+            type="text"
+            name="title"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
         </ModalBody>
         <ModalFooter>
-          <Button color="primary" onClick={toggle}>
-            Do Something
+          <Button color="primary" onClick={handleAddEvent}>
+            Save changes
           </Button>{" "}
           <Button color="secondary" onClick={toggle}>
             Cancel
