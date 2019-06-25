e84007da70e935d9694c56504003b816fd22bc75
buivanhuu2017 <buivanhuu2017@gmail.com>
2019-06-25 17:11:25 +0700

    Appointment Schedule

--- src/containers/AppointmentSchedule/index.js
+++ src/containers/AppointmentSchedule/index.js
@@ -1,45 +1,24 @@
 import React, { useState } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
-import {
-  Modal,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-  Button,
-  Input
-} from "reactstrap";
+import { useTranslation } from "react-i18next";
 
 const localizer = momentLocalizer(moment);
-const myEventsInit = [];
 
 function AppointmentSchedule() {
-  const [modal, setModal] = useState(false);
-  const [backdrop] = useState(true);
-  const [events, setEvents] = useState(myEventsInit);
-  const [eventSelect, setEventSelect] = useState(null);
-  const [title, setTitle] = useState("");
-
-  function toggle() {
-    setModal(!modal);
-  }
+  const { t } = useTranslation();
+  const [events, setEvents] = useState([]);
 
   function handleSelectSlot(e) {
-    const event = {
-      start: e.start,
-      end: e.end
-    };
-    setEventSelect(event);
-    setModal(true);
-  }
-
-  function handleAddEvent() {
-    let eventSelectWithText = eventSelect;
-    eventSelectWithText.title = title;
-    setEvents(events => events.concat([eventSelectWithText]));
-    setEventSelect(null);
-    setTitle("");
-    setModal(false);
+    const title = window.prompt(t("New Event name"));
+    if (title) {
+      const event = {
+        start: e.start,
+        end: e.end,
+        title
+      };
+      setEvents(events => events.concat([event]));
+    }
   }
 
   return (
@@ -48,30 +27,11 @@ function AppointmentSchedule() {
         localizer={localizer}
         events={events}
         defaultView="week"
-        style={{ height: "50vh" }}
+        style={{ height: "60vh" }}
         onSelectSlot={handleSelectSlot}
         selectable={true}
         onSelectEvent={e => console.log(e)}
       />
-      <Modal isOpen={modal} toggle={toggle} backdrop={backdrop}>
-        <ModalHeader toggle={toggle}>Đặt lịch</ModalHeader>
-        <ModalBody>
-          <Input
-            type="text"
-            name="title"
-            value={title}
-            onChange={e => setTitle(e.target.value)}
-          />
-        </ModalBody>
-        <ModalFooter>
-          <Button color="primary" onClick={handleAddEvent}>
-            Save changes
-          </Button>{" "}
-          <Button color="secondary" onClick={toggle}>
-            Cancel
-          </Button>
-        </ModalFooter>
-      </Modal>
     </>
   );
 }
