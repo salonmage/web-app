350f80c8e65f48cba1daa01ec8e20edb1de5770e
huubv <huubv@gmail.com>
2019-06-27 16:16:15 +0700

    Add Service

--- src/containers/Services/AddService.js
+++ src/containers/Services/AddService.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import React from "react";
+import { Button, Form, FormGroup, Label, Input, Alert } from "reactstrap";
 import { useTranslation } from "react-i18next";
 
 function AddService() {
@@ -10,22 +10,33 @@ function AddService() {
       <Form method="POST">
         <FormGroup>
           <Label for="name">{t("Tên dịch vụ")}</Label>
-          <Input type="text" name="name" id="name" />
+          <Input type="text" />
         </FormGroup>
 
         <FormGroup>
           <Label for="name">{t("Thời gian phục vụ")}</Label>
-          <Input type="text" name="name" id="name" />
+          <Input type="text" />
         </FormGroup>
 
         <FormGroup>
           <Label for="name">{t("Giá dịch vụ")}</Label>
-          <Input type="text" name="name" id="name" />
+          <Input type="text" />
         </FormGroup>
 
         <FormGroup>
           <Label for="name">{t("Thông tin chi tiết")}</Label>
-          <Input type="textarea" name="name" id="name" />
+          <Input type="textarea" />
+        </FormGroup>
+
+        <FormGroup>
+          <Label for="name">{t("Các bước thực hiện")}</Label>
+          <Input type="text" />
+          <Alert color="info" isOpen={true} toggle={() => console.log(1)}>
+            1. Rửa mặt
+          </Alert>
+          <Alert color="info" isOpen={true} toggle={() => console.log(1)}>
+            2. Tẩy da chết
+          </Alert>
         </FormGroup>
 
         <FormGroup>
@@ -33,7 +44,7 @@ function AddService() {
         </FormGroup>
       </Form>
     </>
-  )
+  );
 }
 
-export default AddService
+export default AddService;
