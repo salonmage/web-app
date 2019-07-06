4041de3bbbe8d3a7f9c81b8d3edae40d4693054d
huubv <huubv@gmail.com>
2019-07-06 07:02:23 +0700

    Add hook useRouter

--- src/containers/Customers/RegisterService.js
+++ src/containers/Customers/RegisterService.js
@@ -12,6 +12,20 @@ import {
 import { useTranslation } from "react-i18next";
 
 function RegisterService() {
+  return (
+    <>
+      <Row>
+        <Col>
+          <ListService />
+          <ListProduct />
+          <Payment />
+        </Col>
+      </Row>
+    </>
+  );
+}
+
+function Payment() {
   const { t } = useTranslation();
 
   const [modal, setModal] = useState(false);
@@ -20,227 +34,221 @@ function RegisterService() {
   }
 
   return (
-    <>
-      <Row>
-        <Col>
-          <FormGroup>
-            <Label>{t("Dịch vụ triển khai tại Sbeauty")}</Label>
-
-            <Row form>
-              <Col md="4">
-                <FormGroup>
-                  <Input type="select">
-                    <option value="">{t("Chọn dịch vụ")}</option>
-                    <option value="1">Chăm sóc da mặt</option>
-                    <option value="2">IPL</option>
-                    <option value="3">PRP</option>
-                  </Input>
-                </FormGroup>
-              </Col>
-
-              <Col md="8">
-                <div className="alert alert-secondary">
-                  <FormGroup row>
-                    <Label className="text-info customLabel">
-                      {t("Giá sản phẩm")}
-                    </Label>
-                    <Label md="10">150.000</Label>
-                  </FormGroup>
-
-                  <FormGroup row className="sbtGroup">
-                    <Label className="text-info customLabel">
-                      {t("Số lần")}
-                    </Label>
-                    <Col md="2">
-                      <Input type="text" />
-                    </Col>
-                    <Label className="text-info customLabel">{t("Giảm giá")}</Label>
-                    <Col md="2" className="item">
-                      <div className="descriptionInput">
-                        <Input type="text" />
-                        <span>%</span>
-                      </div>
-                    </Col>
-                    <Col md="2" className="item">
-                      <div className="descriptionInput">
-                        <Input type="text" />
-                        <span>$</span>
-                      </div>
-                    </Col>
-                  </FormGroup>
-
-                  <FormGroup row>
-                    <Label className="text-info customLabel">{t("Thành tiền")}</Label>
-                    <Label md="10">100.000</Label>
-                  </FormGroup>
-
-                  <FormGroup row>
-                    <Label xs="12">Gói dịch vụ đang được khuyến mãi 15%</Label>
-                  </FormGroup>
-
-                  <FormGroup>
-                    <Button color="primary">{t("Đăng ký")}</Button>
-                  </FormGroup>
-                </div>
-              </Col>
-            </Row>
-          </FormGroup>
-
-          <FormGroup>
-            <Label>{t("Sản phẩm sử dụng tại nhà")}</Label>
-
-            <Row form>
-              <Col md="4">
-                <FormGroup>
-                  <Input type="select">
-                    <option value="">{t("Chọn sản phẩm")}</option>
-                    <option value="1">Chăm sóc da mặt</option>
-                    <option value="2">IPL</option>
-                    <option value="3">PRP</option>
-                  </Input>
-                </FormGroup>
-              </Col>
-
-              <Col md="8">
-                <div className="alert alert-secondary">
-                  <FormGroup row>
-                    <Label className="text-info customLabel">
-                      {t("Giá sản phẩm")}
-                    </Label>
-                    <Label md="10">150.000</Label>
-                  </FormGroup>
-
-                  <FormGroup row className="sbtGroup">
-                    <Label className="text-info customLabel">
-                      {t("Số lượng")}
-                    </Label>
-                    <Col md="2">
-                      <Input type="text" />
-                    </Col>
-                    <Label className="text-info customLabel">{t("Giảm giá")}</Label>
-                    <Col md="2" className="item">
-                      <div className="descriptionInput">
-                        <Input type="text" />
-                        <span>%</span>
-                      </div>
-                    </Col>
-                    <Col md="2" className="item">
-                      <div className="descriptionInput">
-                        <Input type="text" />
-                        <span>$</span>
-                      </div>
-                    </Col>
-                  </FormGroup>
-
-                  <FormGroup row>
-                    <Label className="text-info customLabel">{t("Thành tiền")}</Label>
-                    <Label md="10">100.000</Label>
-                  </FormGroup>
-
-                  <FormGroup row>
-                    <Label md="12">{t("Cách dùng")}</Label>
-                    <Col>
-                      <Input type="textarea" />
-                    </Col>
-                  </FormGroup>
-
-                  <FormGroup row>
-                    <Label md="12">Gói dịch vụ đang được khuyến mãi 15%</Label>
-                  </FormGroup>
-
-                  <FormGroup>
-                    <Button color="primary">{t("Mua")}</Button>
-                  </FormGroup>
-                </div>
-              </Col>
-            </Row>
-          </FormGroup>
-
-          <FormGroup>
-            <Button onClick={toggleModal} color="primary" type="button">
-              {t("Thanh toán")}
-            </Button>{" "}
-            <Button color="secondary" type="button">
-              {t("Cancel")}
-            </Button>
-            <Modal isOpen={modal} toggle={toggleModal} size="lg">
-              <div
-                style={{
-                  padding: "10px",
-                  maxHeight: "300px",
-                  overflowY: "scroll"
-                }}
-              >
-                <Table hover responsive className="table-outline">
-                  <thead className="thead-light">
-                    <tr>
-                      <th>{t("Tên dịch vụ")}</th>
-                      <th>{t("Đơn giá")}</th>
-                      <th>{t("Số lượng")}</th>
-                      <th>{t("Giảm giá")}</th>
-                      <th>{t("Thành tiền")}</th>
-                      <th>{t("Trạng thái")}</th>
-                      <th>{t("Thao tác")}</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>IPL</td>
-                      <td>400.000</td>
-                      <td>3</td>
-                      <td>15%</td>
-                      <td>1.000.000</td>
-                      <td>Đã thanh toán</td>
-                      <td>
-                        <i
-                          style={{ cursor: "pointer" }}
-                          className="fa fa-remove"
-                        />
-                      </td>
-                    </tr>
-                  </tbody>
-                </Table>
-                <Row form>
-                  <Col>
-                    <div className="alert alert-secondary">
-                      <FormGroup row>
-                        <Label className="text-info customLabel">
-                          {t("Tổng")}
-                        </Label>
-                        <Label md="2">2.000.000</Label>
-                        <Label className="text-info customLabel">
-                          {t("Trả trước")}
-                        </Label>
-                        <Col md="2">
-                          <Input />
-                        </Col>
-                        <Label className="text-info customLabel">
-                          {t("Đã trả")}
-                        </Label>
-                        <Label md="2">500.000</Label>
-                        <Label className="text-info customLabel">
-                          {t("Còn lại")}
-                        </Label>
-                        <Label md="2">1.000.000</Label>
-                      </FormGroup>
-                      
-                      <FormGroup>
-                        <Button color="primary" type="button">
-                          {t("Thanh toán")}
-                        </Button>{" "}
-                        <Button color="secondary" type="button">
-                          {t("Cancel")}
-                        </Button>
-                      </FormGroup>
-                    </div>
+    <FormGroup>
+      <Button onClick={toggleModal} color="primary" type="button">
+        {t("Thanh toán")}
+      </Button>{" "}
+      <Button color="secondary" type="button">
+        {t("Cancel")}
+      </Button>
+      <Modal isOpen={modal} toggle={toggleModal} size="lg">
+        <div
+          style={{
+            padding: "10px",
+            maxHeight: "300px",
+            overflowY: "scroll"
+          }}
+        >
+          <Table hover responsive className="table-outline">
+            <thead className="thead-light">
+              <tr>
+                <th>{t("Tên dịch vụ")}</th>
+                <th>{t("Đơn giá")}</th>
+                <th>{t("Số lượng")}</th>
+                <th>{t("Giảm giá")}</th>
+                <th>{t("Thành tiền")}</th>
+                <th>{t("Trạng thái")}</th>
+                <th>{t("Thao tác")}</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>IPL</td>
+                <td>400.000</td>
+                <td>3</td>
+                <td>15%</td>
+                <td>1.000.000</td>
+                <td>Đã thanh toán</td>
+                <td>
+                  <i style={{ cursor: "pointer" }} className="fa fa-remove" />
+                </td>
+              </tr>
+            </tbody>
+          </Table>
+          <Row form>
+            <Col>
+              <div className="alert alert-secondary">
+                <FormGroup row>
+                  <Label className="text-info customLabel">{t("Tổng")}</Label>
+                  <Label md="2">2.000.000</Label>
+                  <Label className="text-info customLabel">
+                    {t("Trả trước")}
+                  </Label>
+                  <Col md="2">
+                    <Input />
                   </Col>
-                </Row>
+                  <Label className="text-info customLabel">{t("Đã trả")}</Label>
+                  <Label md="2">500.000</Label>
+                  <Label className="text-info customLabel">
+                    {t("Còn lại")}
+                  </Label>
+                  <Label md="2">1.000.000</Label>
+                </FormGroup>
+
+                <FormGroup>
+                  <Button color="primary" type="button">
+                    {t("Thanh toán")}
+                  </Button>{" "}
+                  <Button color="secondary" type="button">
+                    {t("Cancel")}
+                  </Button>
+                </FormGroup>
               </div>
-            </Modal>
+            </Col>
+          </Row>
+        </div>
+      </Modal>
+    </FormGroup>
+  );
+}
+
+function ListProduct() {
+  const { t } = useTranslation();
+
+  return (
+    <FormGroup>
+      <Label>{t("Sản phẩm sử dụng tại nhà")}</Label>
+
+      <Row form>
+        <Col md="4">
+          <FormGroup>
+            <Input type="select">
+              <option value="">{t("Chọn sản phẩm")}</option>
+              <option value="1">Chăm sóc da mặt</option>
+              <option value="2">IPL</option>
+              <option value="3">PRP</option>
+            </Input>
           </FormGroup>
         </Col>
+
+        <Col md="8">
+          <div className="alert alert-secondary">
+            <FormGroup row>
+              <Label className="text-info customLabel">
+                {t("Giá sản phẩm")}
+              </Label>
+              <Label md="10">150.000</Label>
+            </FormGroup>
+
+            <FormGroup row className="sbtGroup">
+              <Label className="text-info customLabel">{t("Số lượng")}</Label>
+              <Col md="2">
+                <Input type="text" />
+              </Col>
+              <Label className="text-info customLabel">{t("Giảm giá")}</Label>
+              <Col md="2" className="item">
+                <div className="descriptionInput">
+                  <Input type="text" />
+                  <span>%</span>
+                </div>
+              </Col>
+              <Col md="2" className="item">
+                <div className="descriptionInput">
+                  <Input type="text" />
+                  <span>$</span>
+                </div>
+              </Col>
+            </FormGroup>
+
+            <FormGroup row>
+              <Label className="text-info customLabel">{t("Thành tiền")}</Label>
+              <Label md="10">100.000</Label>
+            </FormGroup>
+
+            <FormGroup row>
+              <Label md="12">{t("Cách dùng")}</Label>
+              <Col>
+                <Input type="textarea" />
+              </Col>
+            </FormGroup>
+
+            <FormGroup row>
+              <Label md="12">Gói dịch vụ đang được khuyến mãi 15%</Label>
+            </FormGroup>
+
+            <FormGroup>
+              <Button color="primary">{t("Mua")}</Button>
+            </FormGroup>
+          </div>
+        </Col>
       </Row>
-    </>
+    </FormGroup>
+  );
+}
+
+function ListService() {
+  const { t } = useTranslation();
+  return (
+    <FormGroup>
+      <Label>{t("Dịch vụ triển khai tại Sbeauty")}</Label>
+
+      <Row form>
+        <Col md="4">
+          <FormGroup>
+            <Input type="select">
+              <option value="">{t("Chọn dịch vụ")}</option>
+              <option value="1">Chăm sóc da mặt</option>
+              <option value="2">IPL</option>
+              <option value="3">PRP</option>
+            </Input>
+          </FormGroup>
+        </Col>
+
+        <Col md="8">
+          <div className="alert alert-secondary">
+            <FormGroup row>
+              <Label className="text-info customLabel">
+                {t("Giá sản phẩm")}
+              </Label>
+              <Label md="10">150.000</Label>
+            </FormGroup>
+
+            <FormGroup row className="sbtGroup">
+              <Label className="text-info customLabel">{t("Số lần")}</Label>
+              <Col md="2">
+                <Input type="text" />
+              </Col>
+              <Label className="text-info customLabel">{t("Giảm giá")}</Label>
+              <Col md="2" className="item">
+                <div className="descriptionInput">
+                  <Input type="text" />
+                  <span>%</span>
+                </div>
+              </Col>
+              <Col md="2" className="item">
+                <div className="descriptionInput">
+                  <Input type="text" />
+                  <span>$</span>
+                </div>
+              </Col>
+            </FormGroup>
+
+            <FormGroup row>
+              <Label className="text-info customLabel">{t("Thành tiền")}</Label>
+              <Label md="10">100.000</Label>
+            </FormGroup>
+
+            <FormGroup row>
+              <Label xs="12">Gói dịch vụ đang được khuyến mãi 15%</Label>
+            </FormGroup>
+
+            <FormGroup>
+              <Button color="primary">{t("Đăng ký")}</Button>
+            </FormGroup>
+          </div>
+        </Col>
+      </Row>
+    </FormGroup>
   );
 }
 
